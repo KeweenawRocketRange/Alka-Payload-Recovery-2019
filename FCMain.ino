4e48e9593d1e51b555089f24f438a69bf6ca12e3
Erik Van Horn <55638458+ErikVH23@users.noreply.github.com>
2019-10-19 01:42:28 -0400

    Add files via upload

--- FCMain.ino
+++ FCMain.ino
@@ -1,6 +1,30 @@
-#include <Adafruit_BMP085.h>
-#include <Wire.h> //to communicate with the BME & MPU
+/*
+  Keweenaw Rocket Range
+  Beyer Alka Challenge 2019
 
+  Flight Computer Main V1.0
+
+  Authors:
+          Erik Van Horn
+          Paul Mahowald
+          Zoe Knoper
+          Chris Norton
+          Ethan Visscher
+          Andrew Frey
+          Erin Dolson
+          Sean Smith
+
+  Description:
+
+  Updates:
+
+
+*/
+
+//******************************************************************************
+#include <Adafruit_BMP085.h>
+#include <Wire.h>
+//******************************************************************************
 
   const int MPU_addr = 0x68;
   const int CONFIG_ACCEL = 0x1C;
@@ -8,27 +32,22 @@
   const int ACCEL_8 = 0b00010000;
   const int ACCEL_2 = 0b00000000;
   const int PWR_MGMT_1 = 0x6B;
-  const int button = 4;        //Arm button
-  const int rLED = 5;            //Red LED
-  const int gLED = 6;            //Green LED
-  const int beep = 7;            //Buzzer
- 
-  int sampleRate = 10;  //Interval at which average is calculated
-  float maxAlt;         //Max altitude measured
-  int count = 0;
+  const int button = 4;               //Arm button
+  const int rLED = 5;                 //Red LED
+  const int gLED = 6;                 //Green LED
+  const int beep = 7;                 //Buzzer
+
+  int sampleRate = 10;                //Interval at which average is calculated
+  float maxAlt;                       //Max altitude measured
 
   float AcX;
-  float last10AcX[10]; // last 10 measurements of AcX 
-  float avgAcX; // average of the last 50 values
-  float thresholdG = 2; // the threshold g force for detecting launch, should be set to 5
-  float AcY;
-  float AcZ;
-  float Tmp;
-  float GyX;
-  float GyY;
-  float GyZ;
+  float last10AcX[10];                // last 10 measurements of AcX
+  float avgAcX;                       // average of the last 50 values
+  float thresholdG = 2;               // the threshold g force for detecting launch, should be set to 5
+  float AcY;                          //Acceleration in Y direction
+  float AcZ;                          //Acceleration in the Z direction
 
-  bool launched = false;
+  bool launched = false;              //Status of launch
 
   //UI variables
   const int buttonPin = 4;            // Button pin
@@ -43,47 +62,51 @@
   unsigned long startTime;            // Used for a timer
 
   //Detect Apogee vars
-  float altValues[10];
-  float averageAlt[10];
-  int altCheck = 0;
-  int altThreshold = 10;
-  float arraySum = 0;
-  int distanceFromApogee = 1; //meters below apogee
+  float altValues[10];                //Array of 10 raw altitude values
+  float averageAlt[10];               //Array of 10 average altitude values
+  float arraySum = 0;                 //Sum of raw altitudes used to calculate average
+  int distanceFromApogee = 1;         //Meters below apogee
 
-  //motor control
-  const int MOTOR_FORWARD = 10;
-  const int MOTOR_REVERSE = 11;
-  const int motor_time_forward = 5000;
-  const int motor_time_reverse = 1000;
-  
-  Adafruit_BMP085 bmp;
-  
+  //Motor control
+  const int MOTOR_FORWARD = 10;       //Set to high to drive motor forward
+  const int MOTOR_REVERSE = 11;       //Set to high to drive motor in reverse
+  const int motor_time_forward = 5000;//Time motor drives forward
+  const int motor_time_reverse = 1000;//Time motor drives in reverse
+
+  Adafruit_BMP085 bmp;                //Instance of bmp Adafruit BMP180 class
+
+//******************************************************************************
+
 void setup() {
+
   pinMode(button, INPUT);
   digitalWrite(button, HIGH);
   pinMode(gLED, OUTPUT);
   pinMode(rLED, OUTPUT);
   pinMode(beep, OUTPUT);
   pinMode(A0, INPUT);
+
   Wire.begin();
   Wire.beginTransmission(MPU_addr);
-  Wire.write(PWR_MGMT_1);  // PWR_MGMT_1 register
-  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.write(PWR_MGMT_1);             // PWR_MGMT_1 register
+  Wire.write(0);                      // set to zero (wakes up the MPU-6050)
   Wire.endTransmission(true);
   Wire.beginTransmission(MPU_addr);
-  Wire.write(CONFIG_ACCEL); //Accel config register
-  Wire.write(ACCEL_16); //Sets measurment range to +- 16g
+  Wire.write(CONFIG_ACCEL);           //Accel config register
+  Wire.write(ACCEL_16);               //Sets measurment range to +- 16g
   Wire.endTransmission(true);
 
    Serial.begin(9600);
-   
+
  if (!bmp.begin())
  {
     Serial.println("BMP180 sensor not found");
     while (1) {}
  }
+
+//******************************************************************************
  // Setup pins
-   pinMode(buttonPin, INPUT); 
+   pinMode(buttonPin, INPUT);
    pinMode(gLedPin, OUTPUT);
    pinMode(rLedPin, OUTPUT);
    pinMode(buzzerPin, OUTPUT);
@@ -102,7 +125,7 @@ void setup() {
    while(inSetup == true){
       // Loop to strictly look for button presses
       while(checkButtonPresses == false){
-        buttonState = digitalRead(buttonPin); 
+        buttonState = digitalRead(buttonPin);
         if (buttonState != lastButtonState) { // This if statement is what detects a button press
           buttonCounter++;
           delay(250); // Avoid pressing button too many times on accident
@@ -112,7 +135,7 @@ void setup() {
             inButtonSequence = true;
             startTime = millis(); // Start timer
           }
-        }  
+        }
 
         // Keep looping through to count how many times the button has been pressed until the 3 second timer runs out
         if(((millis() - startTime) >= 3000) && inButtonSequence == true){
@@ -120,7 +143,7 @@ void setup() {
         }
       }
 
-      // Checking button presses 
+      // Checking button presses
       // Checks how many times the button was pressed over the last 3 seconds and execute the corresponding statement
       if (checkButtonPresses == true) {
         if(buttonCounter == 1) {
@@ -150,24 +173,7 @@ void setup() {
    digitalWrite(gLedPin, HIGH);
 }
 
-// Function to activate the buzzer
-// buzzerDuration: How long each buzz will last
-// buzzerAmount: How many times it will buzz
-void buzzer(int buzzerDuration, int buzzerAmount) {
-  for(int i = 0; i < buzzerAmount; i++){
-    digitalWrite(buzzerPin, HIGH);
-    delay(buzzerDuration);
-    digitalWrite(buzzerPin, LOW);
-    delay(250);
-  }
-}
-
-// Function to reverse motor
-void reverseMotor(){
-  digitalWrite(MOTOR_REVERSE, HIGH);
-  delay(motor_time_reverse);
-  digitalWrite(MOTOR_REVERSE, LOW);
-}
+//******************************************************************************
 
 void loop(){
   retrieveDataBMP();
@@ -183,24 +189,46 @@ void loop(){
   }
 }
 
+//******************************************************************************
+// Function to activate the buzzer
+// buzzerDuration: How long each buzz will last
+// buzzerAmount: How many times it will buzz
+void buzzer(int buzzerDuration, int buzzerAmount) {
+  for(int i = 0; i < buzzerAmount; i++){
+    digitalWrite(buzzerPin, HIGH);
+    delay(buzzerDuration);
+    digitalWrite(buzzerPin, LOW);
+    delay(250);
+  }
+}
+
+//******************************************************************************
+
+// Function to reverse motor
+void reverseMotor(){
+  digitalWrite(MOTOR_REVERSE, HIGH);
+  delay(motor_time_reverse);
+  digitalWrite(MOTOR_REVERSE, LOW);
+}
+
+//******************************************************************************
+
 void retrieveDataMPU(){
 
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
   Wire.requestFrom(MPU_addr, 14, true);  // request a total of 14 registers
-  
+
   AcX = Wire.read() << 8 | Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
   AcY = Wire.read() << 8 | Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
   AcZ = Wire.read() << 8 | Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-  Tmp = Wire.read() << 8 | Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-  GyX = Wire.read() << 8 | Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-  GyY = Wire.read() << 8 | Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-  GyZ = Wire.read() << 8 | Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
 
-  Serial.println(AcX /2048 );
-  
+  //Serial.println(AcX /2048 );
+
 }
+
+//******************************************************************************
 
 void retrieveDataBMP(){
 
@@ -214,12 +242,14 @@ void retrieveDataBMP(){
 
 }
 
+//******************************************************************************
+
 void calcMaxAlt(){
     for(int i = 0; i < sampleRate - 1; i++){
       altValues[i] = altValues[i+1];
   }
   altValues[sampleRate-1] = bmp.readAltitude(101500);
-  
+
   for(int i = 0; i < sampleRate; i++){
     arraySum += altValues[i];
   }
@@ -242,20 +272,24 @@ void calcMaxAlt(){
       delay(motor_time_forward);
       digitalWrite(MOTOR_FORWARD, LOW);
       while(true){
-        flashNbeep(); 
+        flashNbeep();
       }
   }
-    arraySum = 0; 
+    arraySum = 0;
 }
+
+//******************************************************************************
 
 bool isFalling(){
   for(int i = 0; i < sampleRate; i++){
-    if(!((maxAlt - averageAlt[i]) > distanceFromApogee)){ 
+    if(!((maxAlt - averageAlt[i]) > distanceFromApogee)){
       return(false);
     }
   }
   return(true);
 }
+
+//******************************************************************************
 
 void calcAvg(){
   int size  = 10;
@@ -277,7 +311,10 @@ void calcAvg(){
   Serial.print("Average: ");
   Serial.println(avgAcX);
 }
-boolean detectLaunch (){  
+
+//******************************************************************************
+
+boolean detectLaunch (){
   if (avgAcX > thresholdG || -avgAcX > thresholdG){
     Serial.println("LAUNCH DETECTED!");
     buzzer(500, 1);
@@ -288,8 +325,10 @@ boolean detectLaunch (){
     return true;
   }
   Serial.println("Not launched");
-  return false; 
+  return false;
 }
+
+//******************************************************************************
 
 void flashNbeep(){
   digitalWrite(rLED, HIGH);
@@ -302,3 +341,5 @@ void flashNbeep(){
   delay(100);
   //Serial.println(maxAlt);
 }
+
+//******************************************************************************
