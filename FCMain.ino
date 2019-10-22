996af4687fa0c564ce78b0333ef0319c8d9db78b
Erik Van Horn <55638458+ErikVH23@users.noreply.github.com>
2019-10-22 11:17:30 -0400

    Add files via upload

--- FCMain.ino
+++ FCMain.ino
@@ -1,9 +1,8 @@
+
 /*
   Keweenaw Rocket Range
   Beyer Alka Challenge 2019
-
   Flight Computer Main V1.0
-
   Authors:
           Erik Van Horn
           Paul Mahowald
@@ -13,17 +12,14 @@
           Andrew Frey
           Erin Dolson
           Sean Smith
-
   Description:
-
   Updates:
-
-
 */
 
 //******************************************************************************
 #include <Adafruit_BMP085.h>
 #include <Wire.h>
+#include <EEPROM.h>
 //******************************************************************************
 
   const int MPU_addr = 0x68;
@@ -33,8 +29,8 @@
   const int ACCEL_2 = 0b00000000;
   const int PWR_MGMT_1 = 0x6B;
   const int button = 4;               //Arm button
-  const int rLED = 5;                 //Red LED
-  const int gLED = 6;                 //Green LED
+  const int rLED = 6;                 //Red LED
+  const int gLED = 5;                 //Green LED
   const int beep = 7;                 //Buzzer
 
   int sampleRate = 10;                //Interval at which average is calculated
@@ -50,10 +46,6 @@
   bool launched = false;              //Status of launch
 
   //UI variables
-  const int buttonPin = 4;            // Button pin
-  const int rLedPin = 6;              // Red LED pin
-  const int gLedPin = 5;              // Green LED pin
-  const int buzzerPin = 7;            // Buzzer pin
   int buttonState = 0;                // Current state of the button, used to detect button presses
   int lastButtonState = 0;            // Previous state of the button, used to detect button presses
   int buttonCounter = 0;              // Counter for button presses
@@ -73,18 +65,26 @@
   const int motor_time_forward = 5000;//Time motor drives forward
   const int motor_time_reverse = 1000;//Time motor drives in reverse
 
+  int address = 0;                    //Start byte for EEPROM
+
   Adafruit_BMP085 bmp;                //Instance of bmp Adafruit BMP180 class
 
 //******************************************************************************
 
 void setup() {
-
+  // Setup pins
   pinMode(button, INPUT);
   digitalWrite(button, HIGH);
   pinMode(gLED, OUTPUT);
   pinMode(rLED, OUTPUT);
   pinMode(beep, OUTPUT);
   pinMode(A0, INPUT);
+  pinMode(MOTOR_FORWARD, OUTPUT);
+  pinMode(MOTOR_REVERSE, OUTPUT);
+
+  digitalWrite(button, HIGH);
+  digitalWrite(gLED, LOW);
+  digitalWrite(rLED, HIGH);
 
   Wire.begin();
   Wire.beginTransmission(MPU_addr);
@@ -105,72 +105,11 @@ void setup() {
  }
 
 //******************************************************************************
- // Setup pins
-   pinMode(buttonPin, INPUT);
-   pinMode(gLedPin, OUTPUT);
-   pinMode(rLedPin, OUTPUT);
-   pinMode(buzzerPin, OUTPUT);
-   pinMode(MOTOR_FORWARD, OUTPUT);
-   pinMode(MOTOR_REVERSE, OUTPUT);
-   digitalWrite(buttonPin, HIGH);
-   digitalWrite(gLedPin, LOW);
-   digitalWrite(rLedPin, HIGH);
 
    Serial.begin(9600);
 
-   lastButtonState = digitalRead(buttonPin); // Make the inital state of lastButtonState equal the initial state of buttonState
-
-   // Setup loop
-   boolean inSetup = true;
-   while(inSetup == true){
-      // Loop to strictly look for button presses
-      while(checkButtonPresses == false){
-        buttonState = digitalRead(buttonPin);
-        if (buttonState != lastButtonState) { // This if statement is what detects a button press
-          buttonCounter++;
-          delay(250); // Avoid pressing button too many times on accident
-
-          // If detects a button press while not in a button sequence, start the button sequence and start the timer
-          if(inButtonSequence == false){
-            inButtonSequence = true;
-            startTime = millis(); // Start timer
-          }
-        }
-
-        // Keep looping through to count how many times the button has been pressed until the 3 second timer runs out
-        if(((millis() - startTime) >= 3000) && inButtonSequence == true){
-            checkButtonPresses = true;
-        }
-      }
-
-      // Checking button presses
-      // Checks how many times the button was pressed over the last 3 seconds and execute the corresponding statement
-      if (checkButtonPresses == true) {
-        if(buttonCounter == 1) {
-           buzzer(250, 1);
-           Serial.println("1 Button Press");
-        }
-        else if(buttonCounter == 2) {
-           buzzer(250, 2);
-           Serial.println("2 Button Presses");
-           reverseMotor();
-        }
-        else if(buttonCounter == 3) {
-           buzzer(250, 3);
-           Serial.println("3 Button Presses");
-           inSetup = false; // Exit setup loop
-        }
-      }
-
-      // Reset everything and go back to the loop looking strictly for button presses
-      delay(500);
-      buttonCounter = 0;
-      checkButtonPresses = false;
-      inButtonSequence = 0;
-      lastButtonState = digitalRead(buttonPin);
-   }
-
-   digitalWrite(gLedPin, HIGH);
+   userInterface(); // Call to activate the user interface, looks for button presses
+   
 }
 
 //******************************************************************************
@@ -190,14 +129,76 @@ void loop(){
 }
 
 //******************************************************************************
+
+// Function that loops through the user interface, looking for button presses
+// Function is terminated when the button is pressed 3 times within 3 seconds
+// When function is termninated it will never be called again and the rocket will be armed
+void userInterface() {
+  lastButtonState = digitalRead(button); // Make the inital state of lastButtonState equal the initial state of buttonState
+
+   // Setup loop
+   boolean inSetup = true;
+   while(inSetup == true){
+      // Loop to strictly look for button presses
+      while(checkButtonPresses == false){
+        buttonState = digitalRead(button);
+        if (buttonState != lastButtonState) { // This if statement is what detects a button press
+          buttonCounter++;
+          delay(200); // Avoid pressing button too many times on accident
+
+          // If detects a button press while not in a button sequence, start the button sequence and start the timer
+          if(inButtonSequence == false){
+            inButtonSequence = true;
+            startTime = millis(); // Start timer
+          }
+        }
+
+        // Keep looping through to count how many times the button has been pressed until the 3 second timer runs out
+        if(((millis() - startTime) >= 3000) && inButtonSequence == true){
+            checkButtonPresses = true;
+        }
+      }
+
+      // Checking button presses
+      // Checks how many times the button was pressed over the last 3 seconds and execute the corresponding statement
+      if (checkButtonPresses == true) {
+        if(buttonCounter == 1) {
+           buzzer(250, 1);
+           Serial.print("Max altitude:  ");
+           Serial.println(EEPROM.read(address));
+        }
+        else if(buttonCounter == 2) {
+           buzzer(250, 2);
+           //Serial.println("2 Button Presses");
+           reverseMotor();
+        }
+        else if(buttonCounter == 3) {
+           buzzer(250, 3);
+           //Serial.println("3 Button Presses");
+           inSetup = false; // Exit setup loop
+        }
+      }
+
+      // Reset everything and go back to the loop looking strictly for button presses
+      delay(500);
+      buttonCounter = 0;
+      checkButtonPresses = false;
+      inButtonSequence = 0;
+      lastButtonState = digitalRead(button);
+   }
+
+   digitalWrite(gLED, HIGH);
+}
+
+//******************************************************************************
 // Function to activate the buzzer
 // buzzerDuration: How long each buzz will last
 // buzzerAmount: How many times it will buzz
 void buzzer(int buzzerDuration, int buzzerAmount) {
   for(int i = 0; i < buzzerAmount; i++){
-    digitalWrite(buzzerPin, HIGH);
+    digitalWrite(beep, HIGH);
     delay(buzzerDuration);
-    digitalWrite(buzzerPin, LOW);
+    digitalWrite(beep, LOW);
     delay(250);
   }
 }
@@ -271,6 +272,7 @@ void calcMaxAlt(){
       arraySum = 0;
       delay(motor_time_forward);
       digitalWrite(MOTOR_FORWARD, LOW);
+      EEPROM.write(maxAlt,4);
       while(true){
         flashNbeep();
       }
